be14acd729258dfa6c0efdc8a4e96ae68772897b
tcs76321 <trevorclarkstahl@gmail.com>
2019-10-26 13:41:43 -0700

    feature: make files within directory and do from 10 options 7 times randomly, random each time ran

--- prog2/stahltr.buildrooms.c
+++ prog2/stahltr.buildrooms.c
@@ -5,9 +5,13 @@
 #include <string.h>
 #include <fcntl.h>
 
+#include <time.h>
+
 #include <sys/types.h>
 #include <unistd.h>
 #include <sys/stat.h>
+
+
 
 int main (void){
 
@@ -20,18 +24,66 @@ int main (void){
 	// Maximum 64 decimal value, 9,223,372,036,854,775,807 has 19 digits asxa string
 	// 19 + 14 + some breathing room ~= 40
 	char fileTitle[40];
+	char dirTitle[40];
 
 	// Fill that array with the chars we want starting with the hard-codable first parts
-	sprintf(fileTitle, "stahltr.rooms.%d", pid);
+	sprintf(dirTitle, "stahltr.rooms.%d", pid);
 
 	//Try to make directory
 	int statusOmkdir;
-	statusOmkdir = mkdir(fileTitle, S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
+	statusOmkdir = mkdir(dirTitle, 0777);
 
 	// Check if making directory failed
 	if(!(statusOmkdir == 0)){
 	printf("Error while mkdir ing");
 	return 1;
 	}
+
+	// Make rooms files
+	char letters[] = "ABCDEFGHIJKL";
+	int used[11];
+	
+	int j;
+	for( j =0; j < 11; j++ ){
+		used[j] = 9;
+	}
+
+	int iter;
+	iter = 0;
+
+	int file_d;
+	char file_n[] = "roomX";
+
+	int looper, i;
+
+	srand(time(0));
+
+	int randNumb;
+
+	i = 0;
+	for( i ; i < 7 ; i++ ){
+		looper = 1;
+		// Loop that exits when we find an unused letter to change X or what what used last to be next
+		while(looper == 1){
+			// Make random numberi
+			randNumb = rand() % 10;
+			// If number not used keep it and exit loop by setting looper to be 0
+			if(used[randNumb] == 9){
+				looper = 0;
+				//AND ALSO REMEMBER tjat we keep this one so at to not repeat
+				used[randNumb] = 0;
+			}
+		}	
+		file_n[4] = letters[randNumb];
+		sprintf(fileTitle, "./%s/", dirTitle);
+		strcat(fileTitle, file_n);
+		file_d = open(fileTitle, O_RDWR | O_APPEND | O_CREAT);
+		if(file_d < 0){
+			printf("issue creating file within dir");
+			return 1;
+		}
+	}
+
+	// Finish and return 0 because all error prone steps have if statements to validate and if not return 1
 	return 0;	
 }
